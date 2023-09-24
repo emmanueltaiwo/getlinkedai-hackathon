e32aadcb9bff73cbe618db1e3a7aa9ced200d2ac
Emmanuel Taiwo <127106905+realemmanuel@users.noreply.github.com>
2023-09-24 10:43:30 +0100

    fixed registration form

--- src/components/Register/UserRegistrationForm.tsx
+++ src/components/Register/UserRegistrationForm.tsx
@@ -178,8 +178,8 @@ setIsLoading(false);
   required
   className="w-[180px] md:w-full sm:w-[300px] lg:w-[215px] xl:w-[263px] h-[47px] bg-transparent border-2 border-white rounded-[4px] px-4 text-white text-[14px] placeholder:text-gray-600 outline-none"
 >
-  <option value="select">Select your category</option>
-  {categories.map((category) => (
+  <option value="">Select your category</option>
+  {categories.map((category: any) => (
     <option key={category.id} value={category.id}>
       {category.name}
     </option>
@@ -197,7 +197,7 @@ setIsLoading(false);
             id="selectGroupSize"
             className="w-[100px] md:w-full sm:w-[200px] lg:w-[215px] xl:w-[263px] h-[47px] bg-transparent border-2 border-white rounded-[4px] px-4 text-white text-[14px] placeholder:text-gray-600 outline-none"
           >
-            <option value="select">Select</option>
+            <option value="">Select</option>
             <option value="1">1</option>
             <option value="2">2</option>
             <option value="3">3</option>
